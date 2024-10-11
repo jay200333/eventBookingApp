c70c19e3263e587e448163b0689fce1d4b15adf1
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-10-11 15:12:19 +0900

    개발 시 Logcat에 출력하는 것이 목표였던 기능 수정 기존은 Log 파일에 해당 문구들을 출력하는 것이었음.

--- app/src/main/java/com/example/eventbookingapp/config/interceptors/LoggingInterceptor.kt
+++ app/src/main/java/com/example/eventbookingapp/config/interceptors/LoggingInterceptor.kt
@@ -1,5 +1,6 @@
 package com.example.eventbookingapp.config.interceptors
 
+import android.util.Log
 import com.example.eventbookingapp.config.logger
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -8,27 +9,14 @@ class LoggingInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
 
-        logger.debug {
-            "-------------------------------------------------------------\n"
-        }
-
-        logger.debug {
-            "Request URL\n${request.url}\n\n"
-        }
-
-        logger.debug {
-            "Request Body\n${request.body}\n\n"
-        }
+        Log.d("API CALL", "-------------------------------------------------------------\n")
+        Log.d("API CALL", "Request URL\n${request.url}\n\n")
+        Log.d("API CALL", "Request Body\n${request.body}\n\n")
 
         val response = chain.proceed(request)
 
-        logger.debug {
-            "Response body about ${request.url}\n${response.body}\n\n"
-        }
-
-        logger.debug {
-            "-----------------------------END----------------------------\n"
-        }
+        Log.d("API CALL", "Response body about ${request.url}\n${response.body}\n\n")
+        Log.d("API CALL", "-----------------------------END----------------------------\n")
 
         return response
     }
